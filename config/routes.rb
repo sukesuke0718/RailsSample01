b8965c2fd630cf0f471eb5f3f159861169dfcef4
ユウ <sukesuke0718@gmail.com>
2018-10-27 03:42:28 +0900

    user追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'user/info' => 'user#info'
+  get 'user/list' => 'user#list'
+
+  post 'user/info' => 'user#create'
+
   get '/tests/index' => 'tests#index'
   get '/' => 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
