d72632a382409b461a67cd2919ffbb5590cbe41d
ユウ <sukesuke0718@gmail.com>
2018-10-27 15:17:24 +0900

    パラメータの受け取りを修正

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,6 @@
 class UserController < ApplicationController
   def info
+    # 枠を作成
     @users = User.new
   end
 
@@ -7,17 +8,25 @@ class UserController < ApplicationController
     @users = User.new
   end
 
+  # 登録ボタンが押されたときに実行
   def create
-    @user = User.new(user_id: params[:user_id],
-                      user_password: params[:user_password],
-                      user_name: params[:user_name],
-                      user_adress: params[:user_adress],
-                      user_email: params[:user_email],
+    # フォーム画面の情報を取得
+    @user = User.new(user_id: params[:user][:user_id],
+                      user_password: params[:user][:user_password],
+                      user_name: params[:user][:user_name],
+                      user_adress: params[:user][:user_adress],
+                      user_email: params[:user][:user_email],
                       )
+    # データを保存する
     @user.save
+
+    # ユーザー一覧画面へ移動する
+    redirect_to user_list_path
+
   end
 
   def list
+    # テーブルのデータをすべて取得する
     @user = User.all
   end
 
